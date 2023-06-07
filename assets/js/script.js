436b8bde4559520d8d24ed9305b5e44803d54390
ayohanaa <ayohanabispo@gmail.com>
2023-06-07 18:16:50 -0300

    Menu Hamburguer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+
+function menuShow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "assets/images/menu/bx-menu.png";
+    } else{
+        menuMobile.classList.add('open')
+        document.querySelector('.icon').src = "assets/images/menu/bx-x.png";
+    }
+}
+
+
 const toggleDiv = document.getElementById("toggleDiv");
         const buttonToggle = document.getElementById("buttonToggle");
 
@@ -8,4 +21,3 @@ const toggleDiv = document.getElementById("toggleDiv");
                 toggleDiv.style.display = "none";
             }
 }
-
