b33dad81f25831c6da95cfee543d2793b6617182
vinicius-prates <viniprates2211@gmail.com>
2023-06-06 21:49:08 -0300

    fuction to toggle div

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const toggleDiv = document.getElementById("toggleDiv");
+        const buttonToggle = document.getElementById("buttonToggle");
+
+        function buttonClick() {
+            if (toggleDiv.style.display !== "none"){
+                toggleDiv.style.display = "none";
+            } else {
+                toggleDiv.style.display = "block";
+            }
+}
