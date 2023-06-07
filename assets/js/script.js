d3f6a89e2dd4897e4e241a87f3856fd02c6e0154
vinicius-prates <viniprates2211@gmail.com>
2023-06-06 22:24:31 -0300

    on click show video

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@ const toggleDiv = document.getElementById("toggleDiv");
         const buttonToggle = document.getElementById("buttonToggle");
 
         function buttonClick() {
-            if (toggleDiv.style.display !== "none"){
-                toggleDiv.style.display = "none";
-            } else {
+            if (toggleDiv.style.display !== "flex"){
                 toggleDiv.style.display = "flex";
+            } else {
+                toggleDiv.style.display = "none";
             }
 }
